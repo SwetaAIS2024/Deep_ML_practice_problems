4e508e1091c23d3dc30f48ac79c7e7c9f7cc8116
Sweta Pattnaik <e1350675@u.nus.edu>
2025-05-26 16:45:25 +0800

    Create Problem_102_Swish_activation_fuction.py

---
+++ Problem_102_Swish_activation_fuction.py
@@ -0,0 +1,23 @@
+import math
+import numpy as np
+
+def sigmoid(x: float) -> float:
+    result_s = 1 / (1 + math.exp(-x))
+    #result_s = round(result_s, 4)
+    return result_s
+
+
+def swish(x: float) -> float:
+	"""
+	Implements the Swish activation function.
+
+	Args:
+		x: Input value
+
+	Returns:
+		The Swish activation value
+	"""
+	# Your code here
+    result_swish = x * sigmoid(x)
+    result_swish =  round(result_swish, 4)
+	return result_swish
